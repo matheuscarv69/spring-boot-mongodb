a4f4954eb4eb861c1ec89e88016c8768bf188a14
matheuscarv69 <matheus9126@gmail.com>
2021-01-14 18:08:26 -0400

    Obtendo um post por id

---
+++ src/main/java/com/github/matheuscarv69/services/PostService.java
@@ -0,0 +1,22 @@
+package com.github.matheuscarv69.services;
+
+import com.github.matheuscarv69.domain.Post;
+import com.github.matheuscarv69.repository.PostRepository;
+import com.github.matheuscarv69.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
