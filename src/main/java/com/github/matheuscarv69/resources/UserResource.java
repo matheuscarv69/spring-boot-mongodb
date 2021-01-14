252f3170bbe5e6bbe5b050453fec7ca371152485
matheuscarv69 <matheus9126@gmail.com>
2021-01-14 16:56:19 -0400

    Obtendo um usuário por id

--- src/main/java/com/github/matheuscarv69/resources/UserResource.java
+++ src/main/java/com/github/matheuscarv69/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.github.matheuscarv69.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,12 @@ public class UserResource {
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
--- src/main/java/com/github/matheuscarv69/services/UserService.java
+++ src/main/java/com/github/matheuscarv69/services/UserService.java
@@ -2,10 +2,12 @@ package com.github.matheuscarv69.services;
 
 import com.github.matheuscarv69.domain.User;
 import com.github.matheuscarv69.repository.UserRepository;
+import com.github.matheuscarv69.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,4 +19,8 @@ public class UserService {
         return repository.findAll();
     }
 
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
