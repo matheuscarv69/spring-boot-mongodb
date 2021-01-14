eeb929bd6ab079cf788288791e59d5d5743013c0
matheuscarv69 <matheus9126@gmail.com>
2021-01-14 15:16:44 -0400

    Project created

---
+++ src/main/java/com/github/matheuscarv69/SpringBootEMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.github.matheuscarv69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/matheuscarv69/SpringBootEMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheuscarv69;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
